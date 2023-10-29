85faba3f0196751500ae74321471d614f9024f6a
Dakolo14 <ajosedare4u@gmail.com>
2023-10-29 21:16:15 +0100

    fourth commit

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,14 +1,14 @@
 
 /*=============== EMAIL JS FOR APPLICATION FORM ===============*/
 
-const contactForm = document.getElementById('contact-forms'),
+const contactForm = document.getElementById('contact-form'),
       contactMessage = document.getElementById('contact-message')
 
 const sendEmail = (e) => {
     e.preventDefault()
 
     // serviceID - templateID - #form - publicKey
-    emailjs.sendForm('service_z77nypk','template_krsop85','#contact-forms','tJqN3BfW5Ssh4oL8V').then(() => {
+    emailjs.sendForm('service_z77nypk','template_krsop85','#contact-form','tJqN3BfW5Ssh4oL8V').then(() => {
         //Show sent successfully
         contactMessage.textContent = 'Message sent successfully ✅'
 
